66a01aab70e34f5b2948efaabf8db7ebbe7f06e7
Sridhar <sridharkandi01@gmail.com>
2024-06-14 16:40:46 +0530

    Initial Commit

---
+++ MyFramework/MyFramework.h
@@ -0,0 +1,18 @@
+//
+//  MyFramework.h
+//  MyFramework
+//
+//  Created by Srujana reddy on 14/06/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyFramework.
+FOUNDATION_EXPORT double MyFrameworkVersionNumber;
+
+//! Project version string for MyFramework.
+FOUNDATION_EXPORT const unsigned char MyFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyFramework/PublicHeader.h>
+
+
